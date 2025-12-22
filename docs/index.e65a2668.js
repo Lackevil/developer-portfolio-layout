e10909c72c101727bf85ca325a9f8f0a7d97c1bb
Nazar PAsechyi <whiteroot@protonmail.com>
2025-12-22 13:01:25 +0100

    Deploy fix

---
+++ docs/index.e65a2668.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".navigation"),t=document.querySelector(".burger"),o=document.querySelector("#body");t.addEventListener("click",function(){e.classList.toggle("active-navigation"),t.classList.toggle("active-burger"),o.classList.toggle("overflow-hidden")});
+//# sourceMappingURL=index.e65a2668.js.map
